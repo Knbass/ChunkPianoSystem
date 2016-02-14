c92882897bca4db60395e4c8fdd51ebd144c0542
Kentaro Ueda <g2115005@fun.ac.jp>
2016-02-14 23:12:09 +0900

    Initialize repository

---
+++ ChunkPianoSystem_server.js
@@ -0,0 +1,282 @@
+
+var ChunkPianoSystem_server = function(){
+    ///////////////////////////////////////////////
+    /////////////////////////////////////////////// 
+    var constructor, readIoiFile, getIoiMaxMin, getStringTimeOrYear, getChunkDataJsonList,
+        initHttpAndSocketIo,
+        splitedIoi = [],
+        fs = require('fs'),
+        http = require('http'),
+        socketIo = require('socket.io'), 
+        io
+    ;
+    ///////////////////////////////////////////////
+    /////////////////////////////////////////////// 
+    initHttpAndSocketIo = function(){
+        
+        var httpServer, onHttpRequest;
+
+        ///////////////////////////////////////////////
+        /////////////////////////////////////////////// 
+        onHttpRequest = function(req, res){
+            
+            // console.log(req.url);
+            // res.writeHead(200, {'Content-type':'text/plain'});
+            // res.end('hello http server!\n');
+            
+            switch(req.url){
+                case '/js/libraries/jquery.js':
+                    res.writeHead(200, {'Content-Type':'text/javascript'});
+                    data = fs.readFileSync('./js/libraries/jquery.js', 'utf-8');
+                    res.end(data);
+                    break;
+                case '/js/ChunkPianoSystem_client.js':
+                    res.writeHead(200, {'Content-Type':'text/javascript'});
+                    data = fs.readFileSync('./js/ChunkPianoSystem_client.js', 'utf-8');
+                    res.end(data);
+                    break;
+                case '/js/libraries/sweetalert.min.js':
+                    res.writeHead(200, {'Content-Type':'text/javascript'});
+                    data = fs.readFileSync('./js/libraries/sweetalert.min.js', 'utf-8');
+                    res.end(data);
+                    break;
+                case '/css/ChunkPianoSystem_client.css':
+                    res.writeHead(200, {'Content-Type':'text/css'});
+                    data = fs.readFileSync('./css/ChunkPianoSystem_client.css', 'utf-8');
+                    res.end(data);
+                    break;
+                case '/css/sweetalert.css':
+                    res.writeHead(200, {'Content-Type':'text/css'});
+                    data = fs.readFileSync('./css/sweetalert.css', 'utf-8');
+                    res.end(data);
+                    break;
+                case '/scoreImage/TurcoScore.png':
+                    // How to serve an image using nodejs
+                    // http://stackoverflow.com/questions/5823722/how-to-serve-an-image-using-nodejs
+                    res.writeHead(200, {'Content-Type':'image/png'});
+                    data = fs.readFileSync('./scoreImage/TurcoScore.png'); // png なので utf-8 で読み込んではいけない．
+                    res.end(data, 'binary');
+                    break;
+                default:
+                    res.writeHead(200, {'Content-Type':'text/html'});
+                    data = fs.readFileSync('./ChunkPianoSystem.html', 'utf-8');
+                    res.end(data);
+                    break;
+            }
+        };
+        ///////////////////////////////////////////////
+        /////////////////////////////////////////////// 
+        httpServer = http.createServer(onHttpRequest).listen(3003, '127.0.0.1');
+        
+        // socket.io の初期化
+        io = socketIo.listen(httpServer);
+        
+        io.sockets.on('connection', function(socket){
+
+            socket.on('conected', function(data){
+                console.log('server connected.');
+                // console.log(data);
+            });
+            
+            socket.on('chunkSaveReq', function(data){ // data は chunkDataObj
+                
+                var fileName = '';
+                
+                fileName = String() + './ChunkData/ChunkPianoData_' + data.chunkDataObj.userName + '_' + 
+                           getStringTimeOrYear('date') + '_' + getStringTimeOrYear('time') + 
+                           '_practiceDay-' + data.chunkDataObj.practiceDay + '.json'
+                ;
+                
+                data.chunkDataObj = JSON.stringify(data.chunkDataObj); // chunkDataObj を JSONに変換
+                
+                fs.writeFile(fileName, data.chunkDataObj, function(err){
+                   if(err){
+                       console.log(err);
+                       socket.emit('chunkDataSaveRes',{
+                           status: 'error', // status は success, error, sameFileExist
+                           message: 'チャンクデータの保存に\n失敗しました...'
+                       });
+                   }else{
+                       // todo: 既に同じファイル名が存在する時の確認処理を追加
+                       // todo: クライアントに保存を完了した旨の通知を行う
+                       
+                       socket.emit('chunkDataSaveRes',{
+                           status: 'success', // status は success, error, sameFileExist
+                           message: 'チャンクデータの保存を\n完了しました!'
+                       });
+                   }
+                });
+            });
+            
+            socket.on('chunkFileNameReq', function(data){
+                getChunkDataJsonList('./ChunkData/', function(fileNameList, err){
+                    if(err){
+                        
+                    }else{
+                        
+                        // todo: 保存しているファイルがない場合の処理を追加
+                        
+                        socket.emit('chunkFileNameList',{
+                            status: 'success', // status は success, error, sameFileExist
+                            message: 'チャンクデータの保存を\n完了しました!',
+                            fileNameList:fileNameList
+                        });
+                    }
+                });
+            });
+            
+            // io.sockets.emit では自分以外の全員に emit してしまう... 
+            // 参考: http://www.tettori.net/post/852/ , http://blog.choilabo.com/20120320/31
+            // io.sockets.emit  　→ 自分を含む全員にデータを送信する.
+            // socket.broadcast.emit　→ 自分以外の全員にデータを送信する.
+            // socket.emit　      → 自分のみにデータを送信する. socket.emit であることに注意!
+        });
+    };
+    
+    // 指定フォルダのファイル一覧を取得... http://blog.panicblanket.com/archives/2465
+    // readdir は非同期実行なので次処理は callback で渡す．
+    getChunkDataJsonList = function(directryPathGCDJL, callback){
+        fs.readdir(directryPathGCDJL, function(err, files){
+            
+            try{
+            
+                if (err) throw err;
+
+                var chunkDataJsonList = [];
+
+                files.forEach(function (file){
+                    chunkDataJsonList.push(file);
+                });
+
+                // json 拡張子以外のファイルをファイル名リストから削除
+                for (var i in chunkDataJsonList){
+
+                    var substrString;
+
+                    // 文字列を末尾から指定分切り抜く... https://syncer.jp/javascript-reverse-reference/how-to-use-substr
+                    substrString = chunkDataJsonList[i].substr( (chunkDataJsonList[i].length - 4) , (chunkDataJsonList[i].length - 1) );
+
+                    // 指定位置の要素を削除... https://syncer.jp/javascript-reverse-reference/array-remove
+                    if(substrString != 'json'){
+                        chunkDataJsonList.splice( i , 1 ) ;
+                    }
+                }
+                
+                callback(chunkDataJsonList);
+            }catch(e){
+                callback(chunkDataJsonList, e);
+            }
+        });
+    };
+    ///////////////////////////////////////////////
+    /////////////////////////////////////////////// 
+    getStringTimeOrYear = function(mode){ // 'date' または 'time'
+        
+        var date = new Date();
+        
+        if(mode == undefined){
+            console.error('getStringTime needs argument');
+        }else if(mode == 'time'){
+
+            var hour = date.getHours(),
+                minutes = date.getMinutes(),
+                seconds = date.getSeconds(),
+                milliseconds = date.getMilliseconds()
+            ;
+                    
+            if(hour < 10) hour = '0' + hour;
+            if(minutes < 10) minutes = '0' + minutes; 
+            if(seconds < 10) seconds = '0' + seconds;
+            if(milliseconds < 10){
+                milliseconds = '000' + milliseconds;
+            }else if (milliseconds < 100){
+                milliseconds = '00' + milliseconds;
+            }else if (milliseconds < 1000){
+                milliseconds = '0' + milliseconds;
+            }
+
+            return String() + hour + '-' + minutes + '-' + seconds + '-' + milliseconds;
+        }else if(mode == 'date'){
+            var month = date.getMonth()+1,
+                day = date.getDate(),
+                year = date.getFullYear()
+            ;
+            return String() + year + '-' + month + '-' + day;
+        }
+    };
+    ///////////////////////////////////////////////
+    /////////////////////////////////////////////// 
+    readIoiFile = function(fileName){
+        
+        var ioi;
+        
+        ioi = fs.readFileSync('./files/' + fileName, 'utf-8');
+
+        splitedIoi = ioi.split('\n');
+
+        for(var i in splitedIoi){
+           splitedIoi[i] = parseInt(splitedIoi[i], 10);
+        }
+    };
+    ///////////////////////////////////////////////
+    /////////////////////////////////////////////// 
+    getIoiMaxMin = function(callback){
+        
+        try{
+            var ioiMax = 0,
+                ioiMin = 0
+            ;
+
+            // 比較する初期値として splitedIoi の先頭要素を代入
+            ioiMax = splitedIoi[0]; 
+            ioiMin = splitedIoi[0];
+
+            for(var i in splitedIoi){
+
+                // 最大値を抽出
+                if(ioiMax < splitedIoi[i]){
+                    ioiMax = splitedIoi[i];
+                }
+                // 最小値を抽出
+                if(ioiMin > splitedIoi[i]){
+                    ioiMin = splitedIoi[i];
+                }            
+            }
+            
+            if(ioiMax == undefined || ioiMin == undefined){
+                throw new Error('readIoiFile を先に実行すべし');
+            }
+
+            callback({ioiMax: ioiMax, ioiMin: ioiMin}); // callback の実体は main 関数の getIoiMaxMin 引数に与えた無名関数
+            
+        }catch(e){ // もしエラーを検出したら: e はただの変数名
+            console.log(e);
+        }
+    };
+    ///////////////////////////////////////////////
+    /////////////////////////////////////////////// 
+    constructor = function(){
+        initHttpAndSocketIo();
+    };
+    ///////////////////////////////////////////////
+    /////////////////////////////////////////////// 
+    return {constructor:constructor, readIoiFile:readIoiFile, getIoiMaxMin:getIoiMaxMin};
+};
+///////////////////////////////////////////////
+/////////////////////////////////////////////// 
+///////////////////////////////////////////////
+/////////////////////////////////////////////// 
+///////////////////////////////////////////////
+(function main(){
+    
+    var hhs = ChunkPianoSystem_server(), 
+        ioiMaxMin
+    ;
+    
+    hhs.constructor();
+    // hhs.readIoiFile('ioi.txt');
+    // hhs.getIoiMaxMin(function(ioiMaxMinObj){ // ioiMaxMinObj には getIoiMaxMin 関数から与えられた引数が格納されている
+       // console.log(ioiMaxMinObj);
+    // });
+})();
+
---
+++ js/ChunkPianoSystem_client.js
@@ -0,0 +1,365 @@
+var ChunkPianoSystem_client = function(){
+    ///////////////////////////////////////////////
+    ///////////////////////////////////////////////
+    var constructor, initDomAction, createChunkDom, initSocketIo,
+        chunkDrawingArea = $('#chunkDrawingArea'), socketIo, 
+        patternChunkCount = 0, 
+        // todo: 保存データから chunk を再描画するには 保存時に patternChunkCount も保存し，再描画時に復元しなければいけない．
+        //       復元時は patternChunkCount の最大値を計算し，新しい Chunk の id を最大値よりも大きい値にする．
+        domUtil = ChunkPianoSystem_client.utility(),
+        chunkDataObj = {
+            userName:null,
+            chunkData:{},
+            practiceDay:null
+        }
+    ;
+    ///////////////////////////////////////////////
+    ///////////////////////////////////////////////
+    // todo: チャンクを複数に分けて描画した際の link を指定する引数 parentChunk を追加
+    // createChunkDom = function(left, top, width, height, chunkType){
+    createChunkDom = function(chunkPropCCD){ 
+        // Chunk のサイズが 0 の時には Chunk を描画しない．
+        if((chunkPropCCD.width  != 0 && chunkPropCCD.width  != '0' && chunkPropCCD.width  != null && chunkPropCCD.width  != undefined ) ||
+           (chunkPropCCD.height != 0 && chunkPropCCD.height != '0' && chunkPropCCD.height != null && chunkPropCCD.height != undefined )
+          ){
+    
+            var chunkDom, chunkDomId, chunkDomDelBtn; 
+            
+            // マウスドラッグの x 方向がマイナス方向だった時に正しく描画するための処理．
+            if(chunkPropCCD.width < 0){ 
+                chunkPropCCD.left += chunkPropCCD.width;
+                chunkPropCCD.width = Math.abs(chunkPropCCD.width); // Math.abs() は絶対値を返す．
+            }
+            // マウスドラッグの y 方向がマイナス方向だった時
+            if(chunkPropCCD.height < 0){ 
+                chunkPropCCD.top += chunkPropCCD.height;
+                chunkPropCCD.height = Math.abs(chunkPropCCD.height);
+            }
+
+            chunkDomId = String() + chunkPropCCD.chunkType + 'Chunk_' + patternChunkCount;
+            chunkDom = $('<div class="chunk pattern" id="' + chunkDomId + '"></div>');
+
+            chunkDom.css({ // jQuery で dom の css を変更するときの書法
+                'top'   : chunkPropCCD.top    + 'px',
+                'left'  : chunkPropCCD.left   + 'px',
+                'width' : chunkPropCCD.width  + 'px',
+                'height': chunkPropCCD.height + 'px'
+            });
+
+            domUtil.appendDruggAndDropEvent(chunkDom, chunkDrawingArea);
+            
+            chunkDomDelBtn = $('<div class="chunkDeleteButton" id="' + chunkDomId +'_DeleteButton">' + 
+                                    '<p class="chunkDeleteButtonFont">×</p>' + 
+                               '</div>'
+                              )
+            ;
+            
+            chunkDomDelBtn.click(function(){
+                var parentChunkDom = $(this).parent(),
+                    parentChunkDomId = parentChunkDom[0].id
+                ;
+                parentChunkDom.remove();
+                // html の chunkDom の削除と同時に オブジェクトのデータ構造にも chunkDom を削除．
+                delete chunkDataObj.chunkData[parentChunkDomId];
+                console.log(chunkDataObj);
+            });
+            
+            chunkDom.append(chunkDomDelBtn);
+            // html への chunkDom の追加と同時に オブジェクトのデータ構造にも chunkDom を追加．
+            chunkDataObj.chunkData[chunkDomId] = {
+                left       : chunkPropCCD.left,
+                top        : chunkPropCCD.top,
+                width      : chunkPropCCD.width,
+                height     : chunkPropCCD.height,
+                chunkType  : chunkPropCCD.chunkType,
+				parentChunk: null
+            };
+            console.log(chunkDataObj);
+            
+            chunkDrawingArea.append(chunkDom);
+
+            if(chunkPropCCD.chunkType == 'pattern'){
+                patternChunkCount++; // todo: phraseChunk, hardChunk 描画時のカウンティング処理を追加
+            }
+        }
+    };
+    ///////////////////////////////////////////////
+    /////////////////////////////////////////////// 
+    initSocketIo = function(){
+        
+        // socketIo = io.connect('http://127.0.0.1:3001');
+        socketIo = io.connect();
+        
+        socketIo.on('connect', function () { 
+            socketIo.emit('conected', {data:0});
+        });
+        
+        socketIo.on('disconnect', function(client){            
+	    });
+        
+        socketIo.on('chunkDataSaveRes', function(data){
+            swal(data.message, '', data.status);
+        });
+        
+        socketIo.on('chunkFileNameList', function(data){
+            console.info(data.fileNameList);
+            
+            swal({
+                title: '読み込むチャンクデータを<br>指定してください!',
+                text: '<select class="pullDownMenu" id="chunkDataSelectMenu"><option value = "ueda">uedaData</option></select>',
+                type: 'info',
+                html: true,
+                showCancelButton: false,
+                closeOnConfirm: false,
+                showLoaderOnConfirm: true,
+            }, function(){
+                setTimeout(function () {
+                    swal('データの読み込みを完了しました!');
+                    // swal.close();
+                }, 1000);
+            }); 
+            
+        });
+        
+        $(window).unload(function(){
+        });        
+    };
+    ///////////////////////////////////////////////
+    /////////////////////////////////////////////// 
+    initDomAction = function(callback){
+        var upperFrame = $('#upperFrame'),        
+            saveChunkButton = $('#saveChunkButton'),
+            fileLoadButton = $('#fileLoadButton'),
+            displayTexitButton = $('#displayTexitButton'),
+            alertText = $('.textInput#alertText'),
+            textArea = $('#textArea'),
+            beforeColor = '',
+            isChunkDrawing = false,
+            chunkDrawingAreaMouseDowmPosX = 0,
+            chunkDrawingAreaMouseDowmPosY = 0
+        ;
+        
+        // todo: 標準 prompt は利用せず，SweetAlert を利用: http://t4t5.github.io/sweetalert/
+        // chunkDataObj.userName = window.prompt('ChunkPianoSystem へようこそ!\n ユーザー名を入力してください...', '');  
+        // console.log(chunkDataObj);
+        
+        ///////////////////////////////////////////////
+        ///////////////////////////////////////////////
+        // Chunk 描画処理．mousedown 時に描画開始位置を取得し，mouseup 時に描画終了位置を取得する．
+        chunkDrawingArea.mousedown(function(e){
+            chunkDrawingAreaMouseDowmPosX = parseInt(e.offsetX, 10);
+            chunkDrawingAreaMouseDowmPosY = parseInt(e.offsetY, 10);
+            isChunkDrawing = true;
+        });
+        ///////////////////////////////////////////////
+        /////////////////////////////////////////////// 
+        chunkDrawingArea.mouseup(function(e){
+            
+            if(isChunkDrawing){
+                var chunkSizeX = 0,
+                    chunkSizeY = 0,
+                    chunkProperties = {}
+                ;        
+
+                chunkSizeX = parseInt(e.offsetX, 10) - chunkDrawingAreaMouseDowmPosX;
+                chunkSizeY = parseInt(e.offsetY, 10) - chunkDrawingAreaMouseDowmPosY;
+                
+                chunkProperties = {
+                    left       : chunkDrawingAreaMouseDowmPosX,
+                    top        : chunkDrawingAreaMouseDowmPosY,
+                    width      : chunkSizeX,
+                    height     : chunkSizeY,
+                    chunkType  : 'pattern',
+                    parentChunk: null
+                };
+                
+                createChunkDom(chunkProperties);
+                
+                isChunkDrawing = false;
+            }
+        });    
+        ///////////////////////////////////////////////
+        ///////////////////////////////////////////////
+        saveChunkButton.click(function(){
+            
+            if(Object.keys(chunkDataObj.chunkData).length == 0){ // chunk が一つも描画されていない時は保存処理を行わない
+                swal('保存するにはチャンクを\n1つ以上記入してください!', '', 'warning');
+            }else{
+                var practiceDayChecker, swalPromptOption; 
+                
+                practiceDayChecker = function(practiceDay){
+
+                    if(practiceDay == 0 || practiceDay == undefined || practiceDay == null){                        
+                        swal.showInputError('半角数字で練習日を入力してください．');
+                    }else{
+                        // 001 のように不要な 0 が含まれている数値から 0 を除去
+                        practiceDay += String();
+                        practiceDay.replace(new RegExp('^0+'),'');
+                        practiceDay = parseInt(practiceDay, 10);
+                        practiceDay += String();
+                        
+                        if (practiceDay.match(/^[0-9]+$/)){ // 入力値が半角数字の時
+                            chunkDataObj.practiceDay = practiceDay;
+                            socketIo.emit('chunkSaveReq', {chunkDataObj:chunkDataObj});
+                        }else{
+                            swal.showInputError('半角数字で練習日を入力してください．');
+                        }
+                    }
+                };
+                
+                swalPromptOption = {
+                    title: '今日は何日目の練習日ですか?',
+                    type: 'input',
+                    showCancelButton: false,
+                    closeOnConfirm: false,
+                    animation: 'slide-from-top',
+                    inputPlaceholder: '半角数字で練習日を入力してください．'                    
+                };
+                    
+                swal(swalPromptOption, practiceDayChecker);                
+            }
+        });
+        
+        fileLoadButton.click(function(){
+            // console.log('fileLoadButton');
+            
+            // todo: data で userName をサーバに渡し，その userName のファイルだけを req するようにする．
+            socketIo.emit('chunkFileNameReq',{});
+            
+            // var chunkDataSelectMenu = $('');
+            
+            /*
+            swal({
+                title: '読み込むチャンクデータを<br>指定してください!',
+                text: '<select class="pullDownMenu" id="chunkDataSelectMenu"><option value = "ueda">uedaData</option></select>',
+                type: 'info',
+                html: true,
+                showCancelButton: false,
+                closeOnConfirm: false,
+                showLoaderOnConfirm: true,
+            }, function(){
+                setTimeout(function () {
+                    swal('データの読み込みを完了しました!');
+                    // swal.close();
+                }, 1000);
+            });
+            */
+        });
+        
+        if(callback) callback();
+    };
+    ///////////////////////////////////////////////
+    ///////////////////////////////////////////////
+    constructor = function(){
+        initDomAction(initSocketIo);
+    };
+    ///////////////////////////////////////////////
+    ///////////////////////////////////////////////
+    return {constructor:constructor}; // public method
+};
+//////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////
+// todo: 別の js ファイルに分離
+ChunkPianoSystem_client.utility = function(){
+    ///////////////////////////////////////////////
+    /////////////////////////////////////////////// 
+    var appendDruggAndDropEvent, removeDruggAndDropEvent, getElementPosition;
+    ///////////////////////////////////////////////
+    /////////////////////////////////////////////// 
+    appendDruggAndDropEvent = function(elementADADE, parentElement){
+
+        var draggableArea = parentElement,
+            elementMouseOffsetX, elementMouseOffsetY,
+            isOnMouse = false
+        ;
+        ///////////////////////////////////////////////
+        ///////////////////////////////////////////////  
+        elementADADE.mousedown(function(){
+            
+            var elementPosition;
+
+            isOnMouse = true; 
+            
+            elementPosition = getElementPosition(elementADADE);
+            
+            elementMouseOffsetX = event.pageX - elementPosition.offsetLeft;
+            elementMouseOffsetY = event.pageY - elementPosition.offsetTop;            
+        })
+        ///////////////////////////////////////////////
+        ///////////////////////////////////////////////        
+        draggableArea.mousemove(function(){
+            if(isOnMouse){
+                elementADADE.css({'left':event.pageX - elementMouseOffsetX,
+                                   'top':event.pageY - elementMouseOffsetY
+                                  });
+            }
+        });
+        ///////////////////////////////////////////////
+        ///////////////////////////////////////////////        
+        draggableArea.mouseup(function(){
+            isOnMouse = false;
+        });
+        
+    };
+    ////////////////////////////////////////////
+    ////////////////////////////////////////////
+    // private. 引数... 適用するエレメント
+    removeDruggAndDropEvent = function(elementADADE){
+        
+        try{            
+            elementADADE
+                .unbind('mousedown')
+                .unbind('mouseup')
+            ;
+            
+        }catch(e){
+        }
+        
+    };
+    ///////////////////////////////////////////////
+    /////////////////////////////////////////////// 
+    getElementPosition = function(elementGEO){
+        
+        var offset = elementGEO.offset(),
+            position = elementGEO.position(),
+            elementProp = {
+                offsetTop:null,
+                offsetLeft:null,
+                positionTop:null,
+                positionLeft:null,
+                width:null,
+                height:null
+            }
+        ;
+        
+        elementProp.offsetTop = offset.top;
+        elementProp.offsetLeft = offset.left;
+        elementProp.offsetTop = position.top,
+        elementProp.offsetLeft = position.left,
+        elementProp.width = elementGEO.width(),
+        elementProp.height = elementGEO.height();
+        
+        return elementProp;
+    };
+    ///////////////////////////////////////////////
+    /////////////////////////////////////////////// 
+    return {appendDruggAndDropEvent:appendDruggAndDropEvent, 
+            removeDruggAndDropEvent:appendDruggAndDropEvent,
+            getElementPosition:getElementPosition
+           }
+    ;
+};
+//////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////
+$(function main(){
+    var hh = ChunkPianoSystem_client();
+    hh.constructor();
+});
