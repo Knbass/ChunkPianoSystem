aa25f184ea31384955855fea9bc0c2e8be58fe15
Kentaro Ueda <g2115005@fun.ac.jp>
2016-03-04 18:00:41 +0900

    デバッグのためにユーザ名入力機能を無効化

--- js/ChunkPianoSystem_client.initDomAction.js
+++ js/ChunkPianoSystem_client.initDomAction.js
@@ -70,7 +70,7 @@ ChunkPianoSystem_client.initDomAction = function(globalMemCPSCIDA){
             }
         };
         
-        swal(swalPromptOptionForUserNameProp, userNameSetter);   
+        // swal(swalPromptOptionForUserNameProp, userNameSetter);   
         ///////////////////////////////////////////////
         ///////////////////////////////////////////////
         // 演奏位置初期化処理
